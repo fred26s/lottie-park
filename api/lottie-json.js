e124a68f939941cf1f29f582f6fae33347803415
jf <fred26s@163.com>
2022-09-30 21:51:54 +0800

    增加supabase.js

--- api/lottie-json.js
+++ api/lottie-json.js
@@ -1,13 +1,14 @@
+import { supabase } from "../supabase";
 
 export default async function handler(request, response) {
-  const { data, error } = await supabase
-  .storage
-  .from('lottie-json')
-  .list('json', {
-    limit: 100,
-    offset: 0,
-    sortBy: { column: 'name', order: 'asc' },
-  })
+  const { data, error } = await supabase.storage
+    .from("lottie-json")
+    .list("json", {
+      limit: 100,
+      offset: 0,
+      sortBy: { column: "name", order: "asc" },
+    });
+  if (error) console.log(error);
   response.status(200).json({
     body: data,
     query: request.query,
