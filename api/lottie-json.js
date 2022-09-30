d91390abe68bd97de62550650f9d4998f7c30397
jf <fred26s@163.com>
2022-09-30 22:54:06 +0800

    right key & url

--- api/lottie-json.js
+++ api/lottie-json.js
@@ -1,4 +1,11 @@
-import { supabase } from "../supabase";
+// import { supabase } from "../supabase";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+console.log(supabaseUrl, supabaseAnonKey);
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default async function handler(request, response) {
   try {
