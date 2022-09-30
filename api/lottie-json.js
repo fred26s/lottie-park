efa9ffb5ee9d135caa2cb44ba66a8aa6aabfd920
jf <fred26s@163.com>
2022-09-30 22:50:40 +0800

    trycatch

--- api/lottie-json.js
+++ api/lottie-json.js
@@ -1,27 +1,31 @@
 import { supabase } from "../supabase";
 
 export default async function handler(request, response) {
-  const { data: notes, error:selectErr } = await supabase
-    .from('profiles')
-    .select('username')
+  try {
+    const { data: notes, error: selectErr } = await supabase
+      .from("profiles")
+      .select("username");
 
-  const { data, error } = await supabase.storage
-    .from("lottie-json")
-    .list("json", {
-      limit: 100,
-      offset: 0,
-      sortBy: { column: "name", order: "asc" },
+    const { data, error } = await supabase.storage
+      .from("lottie-json")
+      .list("json", {
+        limit: 100,
+        offset: 0,
+        sortBy: { column: "name", order: "asc" },
+      });
+    if (error) console.log(error);
+    response.status(200).json({
+      body: {
+        error,
+        data,
+        supabase,
+        notes,
+        selectErr,
+      },
+      query: request.query,
+      cookies: request.cookies,
     });
-  if (error) console.log(error);
-  response.status(200).json({
-    body: {
-      error,
-      data,
-      supabase,
-      notes,
-      selectErr
-    },
-    query: request.query,
-    cookies: request.cookies,
-  });
+  } catch (err) {
+    console.log(`handler err: ${err}`);
+  }
 }
