d4cc7636841ae976a1675b8d34567eb829f7c0e1
jf <fred26s@163.com>
2022-09-30 21:35:41 +0800

    test supabase function

--- api/lottie-json.js
+++ api/lottie-json.js
@@ -1,6 +1,15 @@
-export default function handler(request, response) {
+
+export default async function handler(request, response) {
+  const { data, error } = await supabase
+  .storage
+  .from('lottie-json')
+  .list('json', {
+    limit: 100,
+    offset: 0,
+    sortBy: { column: 'name', order: 'asc' },
+  })
   response.status(200).json({
-    body: request.body,
+    body: data,
     query: request.query,
     cookies: request.cookies,
   });
