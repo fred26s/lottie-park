fb7fd06b0e2a7b70c4373d11448f2069858c80eb
jf <fred26s@163.com>
2022-10-01 12:39:53 +0800

    type1 createSignedUrls

--- api/lottie-json.js
+++ api/lottie-json.js
@@ -5,25 +5,10 @@ export default async function handler(request, response) {
     // * 入参
     const { query } = request;
     const { type, fileName } = query;
-    // if (type == 1) {
-      const { data: notes, error: selectErr } = await supabase
-        .from("profiles")
-        .select("username");
-
-      const { data, error } = await supabase.storage
-        .from('lottie-json')
-        .list("json", {
-          limit: 100,
-          offset: 0,
-          sortBy: { column: "name", order: "asc" },
-        });
-      if (error) console.log(error);
-      const fileNameList = data.map(e => `json/${e.name}`)
-      
-      // 批量获取资源URL
+    if (type == 1) {
       const { signedURLList, createSignedUrlsError } = await supabase.storage
-      .from("lottie-json")
-      .createSignedUrls(fileNameList, 60);
+        .from("lottie-json")
+        .createSignedUrls(['json/lottie-1.json', 'json/lottie-2.json'], 60);
       response.status(200).json({
         data: signedURLList,
         body: {
@@ -36,6 +21,38 @@ export default async function handler(request, response) {
         query: request.query,
         cookies: request.cookies,
       });
+      return;
+    }
+    const { data: notes, error: selectErr } = await supabase
+      .from("profiles")
+      .select("username");
+
+    const { data, error } = await supabase.storage
+      .from("lottie-json")
+      .list("json", {
+        limit: 100,
+        offset: 0,
+        sortBy: { column: "name", order: "asc" },
+      });
+    if (error) console.log(error);
+    const fileNameList = data.map((e) => `json/${e.name}`);
+
+    // 批量获取资源URL
+    const { signedURLList, createSignedUrlsError } = await supabase.storage
+      .from("lottie-json")
+      .createSignedUrls(fileNameList, 60);
+    response.status(200).json({
+      data: signedURLList,
+      body: {
+        createSignedUrlsError,
+        data,
+        supabase,
+        notes,
+        selectErr,
+      },
+      query: request.query,
+      cookies: request.cookies,
+    });
     // } else if (type == 2) {
     //   const { signedURL, error } = await supabase.storage
     //     .from("lottie-json")
