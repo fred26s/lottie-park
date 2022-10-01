daff101db6baf334ad0469fdedd0c2df188fc8cc
jf <fred26s@163.com>
2022-10-01 17:42:09 +0800

    仍使用URL复制逻辑

--- api/lottie-json.js
+++ api/lottie-json.js
@@ -3,28 +3,6 @@ import { supabase } from "../supabase";
 export default async function handler(request, response) {
   try {
     // * 入参
-    const { query } = request;
-    const { type, fileName } = query;
-    if (type == 1) {
-      const { data: signedURLList, error: createSignedUrlsError } =
-        await supabase.storage
-          .from("lottie-json")
-          .createSignedUrls(["json/lottie-1.json", "json/lottie-2.json"], 60);
-      response.status(200).json({
-        data: signedURLList,
-        body: {
-          createSignedUrlsError,
-          supabase,
-        },
-        query: request.query,
-        cookies: request.cookies,
-      });
-      return;
-    }
-    const { data: notes, error: selectErr } = await supabase
-      .from("profiles")
-      .select("username");
-
     const { data, error } = await supabase.storage
       .from("lottie-json")
       .list("json", {
@@ -32,36 +10,30 @@ export default async function handler(request, response) {
         offset: 0,
         sortBy: { column: "name", order: "asc" },
       });
-    if (error) console.log(error);
     const fileNameList = data.map((e) => `json/${e.name}`);
 
-    // 批量获取资源URL
+    // * 批量获取资源URL
     const { data: signedURLList, error: createSignedUrlsError } =
       await supabase.storage
         .from("lottie-json")
         .createSignedUrls(fileNameList, 60);
-    
-        // * 这里默认接口返回的URL list数量，全等于上一个接口查出的所有文件数量
-    const renderDataList = signedURLList.map((url,index) => {
+
+    // * 这里默认接口返回的URL list数量，全等于上一个接口查出的所有文件数量
+    const renderDataList = signedURLList.map((url, index) => {
       return {
-        url,
-        fileName: fileNameList[index]
-      }
-    })
+        url: url.map((e) => e.signedURL),
+        fileName: fileNameList[index],
+      };
+    });
     response.status(200).json({
       data: renderDataList,
-      body: {
-        createSignedUrlsError,
-        signedURLList: signedURLList.map(e => e.signedURL),
-        data,
-        supabase,
-        notes,
-        selectErr,
-      },
-      query: request.query,
-      cookies: request.cookies,
+      msg: 'ok'
     });
   } catch (err) {
     console.log(`handler err: ${err}`);
+    response.status(500).json({
+      data: err,
+      msg: 'err'
+    });
   }
 }
